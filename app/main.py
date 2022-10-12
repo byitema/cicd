158932749c33b2a503f071d1a6c996c192bf6ee6
Artem <artem.bulakh1@gmail.com>
2022-10-12 15:14:11 +0300

    rename functions

--- app/main.py
+++ app/main.py
@@ -18,11 +18,11 @@ async def healthcheck():
 
 
 @app.get("/items", response_model=List[schemas.Item])
-def read_items(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+def list_items(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     items = crud.get_items(db, skip=skip, limit=limit)
     return items
 
 
 @app.post("/items", response_model=schemas.Item)
-def create_item_for_user(item: schemas.ItemCreate, db: Session = Depends(get_db)):
+def create_item(item: schemas.ItemCreate, db: Session = Depends(get_db)):
     return crud.create_user_item(db=db, item=item)
