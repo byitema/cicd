db417a9808a4dd6748e0a894995848e7c2e5d3fc
Artem <artem.bulakh1@gmail.com>
2022-10-12 12:06:08 +0300

    add docker-compose.yml, database handling and items endpoint

--- app/main.py
+++ app/main.py
@@ -1,4 +1,13 @@
-from fastapi import FastAPI
+from typing import List
+
+from fastapi import FastAPI, Depends
+from sqlalchemy.orm import Session
+
+from . import models, schemas, crud
+from .database import engine, get_db
+
+models.Base.metadata.create_all(bind=engine)
+
 
 app = FastAPI()
 
@@ -6,3 +15,14 @@ app = FastAPI()
 @app.get('/healthcheck', status_code=200)
 async def healthcheck():
     return {'message': 'ok'}
+
+
+@app.get("/items", response_model=List[schemas.Item])
+def read_items(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    items = crud.get_items(db, skip=skip, limit=limit)
+    return items
+
+
+@app.post("/items", response_model=schemas.Item)
+def create_item_for_user(item: schemas.ItemCreate, db: Session = Depends(get_db)):
+    return crud.create_user_item(db=db, item=item)
